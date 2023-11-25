b47bccb9c6a57e7de9e3d893c3efd7a412bf6b3c
Krishna-BR <62740264+Krishna-BR@users.noreply.github.com>
2023-11-25 23:57:29 +0530

    Update Message.js

--- effbot-web/src/components/Message.js
+++ effbot-web/src/components/Message.js
@@ -27,7 +27,7 @@ const Message = ({ role, content }) => {
         </div>
         {
           (content.includes("URL:")) ?
-          <img
+          <Image
             src={content.split("URL: ")[1].split("\n\nInstruction")[0]}
             alt="Avatar"
             className="border"
